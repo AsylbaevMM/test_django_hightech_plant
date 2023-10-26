064da2b8df66bea65dbb795bb208268766a1d594
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-10-26 14:00:17 +0300

    0.0.3 Create app accounts, create model MyUser, set PostgreSQL

--- test_django_hightech_plant/settings.py
+++ test_django_hightech_plant/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'test_django_hightech_plant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(os.getenv('DATABASE_NAME')),
+        'USER': str(os.getenv('DATABASE_USER')),
+        'PASSWORD': str(os.getenv('DATABASE_PASSWORD')),
     }
 }
 
@@ -127,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.models.MyUser'
+
