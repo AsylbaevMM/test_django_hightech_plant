1a88fc5d86a31b9d9126c30b8d2030ac63d918d4
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-10-30 02:17:46 +0300

    final

--- app/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MyUser
-
-
-@admin.register(MyUser)
-class PostMyUser(admin.ModelAdmin):
-    list_display = ['id', 'is_superuser', 'username', 'is_active']
